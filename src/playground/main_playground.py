c1d2ffc15cb9d225a24cf629bc07d7e64ffc2004
midegithub <meddi.moumouni@gmail.com>
2025-11-13 15:05:23 +0900

    Add methods to build observation space and reset environment function

--- src/playground/main_playground.py
+++ src/playground/main_playground.py
@@ -89,7 +89,71 @@ class MainPlayground:
         
         return joint_indices
     
+    def get_observation(self):
+        """Get the complete state representation for the agent.
+        State includes:
+        Base position (z-height only) - 1 dim
+        Base orientation (quaternion) - 4 dims
+        Base linear velocity (x,y,z) - 3 dims
+        Base angular velocity (x,y,z) - 3 dims
+        Joint positions (12) -12 dims
+        Joint velocities (12) - 12 dims
+        Total: 1 + 4 + 3 + 3 + 12 + 12 = 35 dimensions"""
     
+        # p.getBasePositionAndOrientation returns (pos_vec3, orn_quat4)
+
+        base_pos, base_orn_quat=p.getBasePositionAndOrientation(self.robot_id,physicsClientId=self.client_id)
+
+
+        #Get linvelvec3, ang velvec3
+
+        base_vel_lin, base_vel_ang= p.getBaseVelocity(self.robot_id, physicsClientId=self.client_id)
+
+        # Get joints states
+
+        joint_states=p.getJointStates(self.robot_id, self.actuated_joint_indices, physicsClientId=self.client_id)
+        joint_pos=[s for s in joint_states]
+        joint_vel=[s for s in joint_states]
+
+
+        # Concatenate all observations
+
+        obs_list= ([base_pos]+
+                   list(base_orn_quat)+
+                   list(base_vel_lin)+
+                   list(base_vel_ang)+
+                   list(joint_pos)+
+                   list(joint_vel))
+        
+        #Set state dim on firsst call
+        if self.state_dim is None:
+            self.state_dim=len(obs_list)
+            print(f"Observation space dimension set to :{self.state_dim}")
+
+        return np.array(obs_list, dtype = np.float32)
+    
+    def reset(self):
+        """ Resets the environment to the starting state """
+        #Reset robot base
+        p.resetBasePositionAndOrientation(
+            self.robot_id, self.start_pos, self.start_orn, physicsClientId=self.client_id
+        )
+
+        p.resetBaseVelocity(self.robot_id,None,None,physicsClientId=self.clien_id)
+
+        for i in self.actuated_joint_indices:
+            p.resetJointState(self.robot_id,i, targetValue=0, targetVelocity=0, physicsClientId=self.client_id)
+
+            #TO DO:  reset the rewards (not yet implemented)
+
+            #Get initial observation
+            return self.get_observation()
+
+            
+
+
+
+
 
 
 
